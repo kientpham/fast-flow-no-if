6d7c25544a203187b4212ff45c9f8b11aa9f5119
Kien Pham <trungkienbk@gmail.com>
2018-09-25 09:34:07 -0700

    Remove comments

--- corelib/src/main/java/com/datatransforming/workflow/AbstractBuilder.java
+++ corelib/src/main/java/com/datatransforming/workflow/AbstractBuilder.java
@@ -14,13 +14,7 @@ import lombok.Setter;
 public abstract class AbstractBuilder<D>
 {
   private AbstractBuilder<D> nextBuilder;  
-
-  /**
-   * This function is to build output package
-   * 
-   * @param inputDTO
-   * @throws DdmfToDdpsException
-   */
+  
   public abstract void execute(AbstractTransactionModel transaction, D builderDTO) throws ServiceException;
 
 }
